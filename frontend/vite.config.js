a0d2e11a82877b5c59707af77154eb7398ac6ee3
SoWiEee <kawiisowieee@gmail.com>
2025-07-08 13:00:15 +0800

    import Quasar

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'; // 引入 Quasar Vite 插件
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    quasar({
+      autoImportComponentCase: 'pascal',
+      transformAssetUrls,
+    }),
+  ],
 })
