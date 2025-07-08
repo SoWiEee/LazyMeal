939b868d12a4b6a90b65b23a570ba62763512831
SoWiEee <kawiisowieee@gmail.com>
2025-07-09 07:20:09 +0800

    fix api path

--- backend/src/routes/watchlist.js
+++ backend/src/routes/watchlist.js
@@ -6,10 +6,7 @@ async function watchlistRoutes(fastify, options) {
 	fastify.get('/search-google', searchGoogle);
 
 	// add restaurant to watchlist
-	fastify.post('/add-to-watchlist', addToWatchlist);
-
-	// import restaurant from google map link
-	fastify.post('/import-from-link', importFromLink);
+	fastify.post('/', addToWatchlist);
 
 	// get user watchlist
 	fastify.get('/', getWatchlist);
