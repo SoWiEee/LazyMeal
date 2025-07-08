d60ee000feb5a943eeed722f73a416dbf88101d2
SoWiEee <kawiisowieee@gmail.com>
2025-07-08 13:48:26 +0800

    fix tab space

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'; // 引入 Quasar Vite 插件
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    quasar({
-      autoImportComponentCase: 'pascal',
-      transformAssetUrls,
-    }),
-  ],
+	plugins: [
+		vue(),
+		quasar({
+			transformAssetUrls,
+		}),
+	],
 })
