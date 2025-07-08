40c3558dc321ad3d75cd4d486a1defa81ca133cf
SoWiEee <kawiisowieee@gmail.com>
2025-07-08 16:46:22 +0800

    let 'LazyMeal' align center

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,14 +3,14 @@
 </script>
 
 <template>
-	<q-layout view="lHh Lpr lff"> <q-header elevated class="bg-primary text-white">
-		<q-toolbar>
-			<q-toolbar-title>
-				Lazy Meal
-			</q-toolbar-title>
-		</q-toolbar>
-    </q-header>
-
+	<q-layout view="lHh Lpr lff">
+		<q-header elevated class="bg-primary text-white">
+			<q-toolbar>
+				<q-toolbar-title class="text-center">
+					Lazy Meal
+				</q-toolbar-title>
+			</q-toolbar>
+    	</q-header>
     <q-page-container> <router-view /> </q-page-container>
 
     </q-layout>
