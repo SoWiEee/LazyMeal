eec5f714e8d64b73b9dc2a7dd4ea0cc9a6cc2c19
SoWiEee <kawiisowieee@gmail.com>
2025-07-14 07:53:28 +0800

    add get location logic

--- frontend/src/components/RestaurantSearch.vue
+++ frontend/src/components/RestaurantSearch.vue
@@ -1,7 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRestaurantStore } from '../stores/restaurantStore'
+import { useNotification } from '../useNotification'
 const restaurantStore = useRestaurantStore()
+const { showNotification } = useNotification()
 
 const searchQuery = ref('麥當勞')
 const userLat = ref(22.6865)	// 預設緯度
@@ -15,6 +17,28 @@ const searchRestaurants = () => {
 		restaurantStore.error = '請輸入餐廳名稱進行搜索。'
 	}
 }
+
+// 獲取使用者地理位置
+const getUserLocationAndSearch = () => {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				userLat.value = position.coords.latitude
+				userLon.value = position.coords.longitude
+				searchRestaurants() // 獲取位置後再搜尋
+			},
+			() => {
+				showNotification({ success: false, message: '無法獲取您的位置，將使用預設地點搜尋。' })
+				searchRestaurants() // 失敗時使用預設位置搜尋
+			}
+		)
+	}
+}
+
+onMounted(() => {
+	searchRestaurants()
+	// getUserLocationAndSearch()
+})
 </script>
 
 <template>
